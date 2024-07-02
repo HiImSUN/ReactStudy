ffdedbd9d419d9978da7ae84214c9cf26747907a
HiImSUN <kshk08@hanyang.ac.kr>
2024-07-03 04:17:13 +0900

    npx create-react-app 을 통해 프로젝트 생성하기

---
+++ react-for-beginners/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={styles.title}>WELCOM to the SHOW</h1>
+      <Button text="Continue"></Button>
+    </div>
+  );
+}
+
+export default App;
