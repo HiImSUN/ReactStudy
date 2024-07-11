f611aea7eb65ac6168cbff19e1bd6028c94d40da
HiImSUN <kshk08@hanyang.ac.kr>
2024-07-12 03:18:05 +0900

    todo리스트 추가와 삭제

--- 7.1ToDoList/todolist_pjt/src/App.js
+++ 7.1ToDoList/todolist_pjt/src/App.js
@@ -2,18 +2,49 @@ import { useState } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
-  const onChange = (event) => setToDo(event.target.value);
+  const [toDoList, setToDoList] = useState([]);
+
+  const onChangeHandler = (event) => {
+    setToDo(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      // 만약 빈 todo가 제출되면 함수 종료
+      return;
+    }
+    setToDoList((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+
+  const deleteBtn = (index) => {
+    setToDoList((currentArray) =>
+      currentArray.filter((item, toDoIndex) => index !== toDoIndex)
+    );
+  };
+
   return (
     <div>
-      <form>
+      <h1>TO-DO LIST ({toDoList.length}개)</h1>
+      <form onSubmit={onSubmit}>
         <input
-          value={toDo}
-          onChange={onChange}
+          onChange={onChangeHandler}
           type="text"
-          placeholder="Write you to-do.."
+          placeholder="Write your todo"
+          value={toDo}
         />
-        <button>Add To Do</button>
+        <button type="submit">작성하기</button>
       </form>
+      <hr />
+      <ul>
+        {toDoList.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteBtn(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
