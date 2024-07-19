ed72a454b625e94119766e2c6267b68f93e125f1
HiImSUN <kshk08@hanyang.ac.kr>
2024-07-20 00:27:39 +0900

    7.5 React Router까지 공부

--- 7.3MovieApp/frontend/src/App.js
+++ 7.3MovieApp/frontend/src/App.js
@@ -1,57 +1,22 @@
-import { useState, useEffect } from "react";
-
-const REF_URL =
-  "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [isLoading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const response = await fetch(REF_URL);
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
   return (
-    <div>
-      {isLoading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div
-              key={movie.id}
-              style={{
-                display: "flex",
-                marginBottom: "20px",
-                alignItems: "top",
-              }}
-            >
-              <img
-                src={movie.medium_cover_image}
-                alt={movie.title}
-                style={{ marginRight: "20px" }}
-              />
-              <div>
-                <h2>{movie.title}</h2>
-                <p>{movie.summary}</p>
-                <ul>
-                  {movie.genres.map((g) => (
-                    <li key={g}>{g}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
